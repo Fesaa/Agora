d890155252f1f5564ecb4e93d08c37e713322843
Fesaa <77553571+Fesaa@users.noreply.github.com>
2025-01-25 17:13:54 +0100

    .NET inti

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,35 @@
+using System.Net;
+using System.Text.Json;
+using API.Exceptions;
+
+namespace API.Middleware;
+
+public class ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+{
+    private static readonly JsonSerializerOptions JsonSerializerOptions = new JsonSerializerOptions
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+    };
+    
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An exception occurred while handling an http request.");
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+
+            var errorMessage = string.IsNullOrEmpty(ex.Message) ? "Internal Server Error" : ex.Message;
+
+            var response = new ApiException(context.Response.StatusCode, errorMessage, ex.StackTrace);
+
+            var json = JsonSerializer.Serialize(response, JsonSerializerOptions);
+
+            await context.Response.WriteAsync(json);
+        }
+    }
+}
