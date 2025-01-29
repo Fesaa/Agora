94db54dfff5f5c159dc4c76dfd43522023a7489f
LehhitAbdel <abdel.lehhit@student.ehb.be>
2025-01-30 00:42:26 +0100

    dashboard with mock data, figuring out components etc (not finished)

--- UI/Web/src/app/dashboard/dashboard.component.ts
+++ UI/Web/src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [TableModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  meetings = [
+    { time: '09:00', office: 'Room 101', participants: ['Alice', 'Bob'] },
+    { time: '11:00', office: 'Room 102', participants: ['Charlie', 'Dana'] },
+    { time: '14:00', office: 'Room 201', participants: ['Eve', 'Frank'] }
+  ];
 
 }
