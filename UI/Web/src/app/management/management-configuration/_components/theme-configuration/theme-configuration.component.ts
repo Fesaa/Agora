325ded62aa2fa517ba29474099751430ac9204bf
Fesaa <77553571+Fesaa@users.noreply.github.com>
2025-02-15 14:23:56 +0100

    Start themes

--- UI/Web/src/app/management/management-configuration/_components/theme-configuration/theme-configuration.component.ts
+++ UI/Web/src/app/management/management-configuration/_components/theme-configuration/theme-configuration.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ThemeService} from '../../../../_services/theme.service';
+import {Theme} from '../../../../_models/theme';
+import {Card} from 'primeng/card';
+import {TranslocoDirective} from '@jsverse/transloco';
+import {Tag} from 'primeng/tag';
+import {ProviderPipe} from '../../../../_pipes/provider.pipe';
+import {TitleCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-theme-configuration',
-  imports: [],
+  imports: [
+    Card,
+    TranslocoDirective,
+    Tag,
+    ProviderPipe,
+    TitleCasePipe
+  ],
   templateUrl: './theme-configuration.component.html',
   styleUrl: './theme-configuration.component.css'
 })
-export class ThemeConfigurationComponent {
+export class ThemeConfigurationComponent implements OnInit{
+
+  themes: Theme[] = [];
+
+  constructor(
+    private themeService: ThemeService,
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.themeService.all().subscribe(themes => this.themes = themes);
+  }
 
 }
