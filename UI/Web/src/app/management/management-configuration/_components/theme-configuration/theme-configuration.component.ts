0fa622502488ae75438bfd384b4d45b349cc40d5
Fesaa <77553571+Fesaa@users.noreply.github.com>
2025-02-15 13:57:28 +0100

    Add simple configuration page
    This will be used to setup things, the dashboard that I added will be
    used to have a quick overview of what's currently going on. And things
    that need to be confirmed / facilities needed / etc

---
+++ UI/Web/src/app/management/management-configuration/_components/theme-configuration/theme-configuration.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-configuration',
+  imports: [],
+  templateUrl: './theme-configuration.component.html',
+  styleUrl: './theme-configuration.component.css'
+})
+export class ThemeConfigurationComponent {
+
+}
