b56a66d596ca50d07e100d2156ec9f3e34935754
Fesaa <77553571+Fesaa@users.noreply.github.com>
2025-02-17 13:47:28 +0100

    Use Docusaurus for documentation

---
+++ UI/documentation/docs/quick-start.md
@@ -0,0 +1,5 @@
+
+
+## What is Agora?
+Agora is an open-source, self-hosted meeting management system. Designed to integrate with your existing
+infrastructure. 
---
+++ UI/documentation/src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,69 @@
+import type {ReactNode} from 'react';
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+type FeatureItem = {
+  title: string;
+  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  description: ReactNode;
+};
+
+const FeatureList: FeatureItem[] = [
+  {
+    title: 'Easy to Use',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+        Agora was designed from the ground up to integrate nicely with existing infrastructure.
+      </>
+    ),
+  },
+  {
+    title: 'Focus on What Matters',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+        Agora lets you focus on getting your work done. Inviting colleges to your calender item? Letting reception known
+          you need something? Agora's got it covered.
+      </>
+    ),
+  },
+  {
+    title: 'Docker',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        Deploy easily with a single docker container, simple and quick.
+      </>
+    ),
+  },
+];
+
+function Feature({title, Svg, description}: FeatureItem) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures(): ReactNode {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
