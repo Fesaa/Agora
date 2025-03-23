60836e2befc3c9c1f8bb9903e5bb1607ba4ec61a
Fesaa <77553571+Fesaa@users.noreply.github.com>
2025-03-23 18:04:16 +0100

    Little work on room, stopping for today

--- UI/Web/src/app/user/meeting-wizard/_components/meeting-wizard-room/meeting-wizard-room.component.ts
+++ UI/Web/src/app/user/meeting-wizard/_components/meeting-wizard-room/meeting-wizard-room.component.ts
@@ -3,13 +3,15 @@ import {Meeting} from '../../../../_models/meeting';
 import {Card} from 'primeng/card';
 import {Button} from 'primeng/button';
 import {TranslocoDirective} from '@jsverse/transloco';
+import {DatePicker} from 'primeng/datepicker';
 
 @Component({
   selector: 'app-meeting-wizard-room',
   imports: [
     Card,
     Button,
-    TranslocoDirective
+    TranslocoDirective,
+    DatePicker
   ],
   templateUrl: './meeting-wizard-room.component.html',
   styleUrl: './meeting-wizard-room.component.css'
